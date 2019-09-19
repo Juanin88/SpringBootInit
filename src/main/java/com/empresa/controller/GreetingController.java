56f2569b1ca0e031f9ba3252720b17112dd207e9
Juan Garfias Vázquez <ing.juan.garfias@gmail.com>
2019-09-18 21:11:42 -0500

    Azure configuración

--- src/main/java/com/empresa/controller/GreetingController.java
+++ src/main/java/com/empresa/controller/GreetingController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name.toUpperCase());
+        model.addAttribute("name", name.toLowerCase());
         return "greeting";
     }
 }
